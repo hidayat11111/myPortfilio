6bc34cf135aed33b85ea575784bf1632b84650ed
Hidayat ali beigh <96649174+hidayat11111@users.noreply.github.com>
2024-09-23 22:52:56 +0530

    UPDATE

--- src/Components/SplashPage.js
+++ src/Components/SplashPage.js
@@ -1,7 +1,8 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { SparklesCore } from "./Sparkles";
 import { useNavigate } from "react-router-dom";
+import BounceLoader from 'react-spinners/BounceLoader';
 
 function SparklesPreview() {
   const nav = useNavigate();
@@ -11,17 +12,15 @@ function SparklesPreview() {
       nav("/LandingPage");
     }, 3000);
   }, [nav]);
-
+  const [loading, setLoading] = useState(true);
   return (
     <div className="h-svh w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <h1 className="md:text-5xl text-3xl lg:text-6xl font-bold font-sans text-center text-white relative z-20">
-        Hidayat Ali
-      </h1>
+       <BounceLoader color="gray" loading={loading} size={60} />
       <div className="w-[40rem] h-40 relative">
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+        {/* <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
         <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" /> */}
 
         <SparklesCore
           background="transparent"
